04840b080366764f8c1e6df42f8ee91613dcc3ab
Roberto <san20086@byui.edu>
2024-10-28 09:50:20 -0600

    Refactor main card to a component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes, css } from 'styled-components';
+
+//Component Imports//
+import ResponsiveModalCard from './assests/components/mainCard/ResponsivelModalCard';
+
 function App(){
   
     return (
       <AppContainer>
-           
+        <ResponsiveModalCard>
+        </ResponsiveModalCard>
       </AppContainer>
     );
 }
+
+
+
+//Style for App container //
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #0B6DA2;
+  padding: 10px;
+`;
+
+export default App; 
   
